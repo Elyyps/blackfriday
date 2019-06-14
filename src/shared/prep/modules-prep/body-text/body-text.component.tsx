0b45f94b5938319ab3b70787c1a6e2cca86c04c4
jekatibor <jeka.tibor@gmail.com>
2019-06-14 15:13:57 +0300

    style: icons, button, body-text

---
+++ src/shared/prep/modules-prep/body-text/body-text.component.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import styles from "./body-text-component.module.scss";
+
+export interface IBodyTextComponentProps {
+  bgcolor?: string;
+  children: any;
+}
+
+const BodyTextComponent = (props: IBodyTextComponentProps) => {
+  const style = {
+    backgroundColor: props.bgcolor,
+    paddingTop: "72px",
+    paddingBottom: "72px"
+  };
+
+  return (
+    <div className={styles["body-text"]} style={style}>
+      <div className="uk-container">{props.children}</div>
+    </div>
+  );
+};
+
+export { BodyTextComponent };
