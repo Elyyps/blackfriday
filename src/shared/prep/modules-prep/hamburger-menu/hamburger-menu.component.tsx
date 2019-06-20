52634f42edd3d6df65b528401b3d7f7242523b9b
jekatibor <jeka.tibor@gmail.com>
2019-06-20 13:55:05 +0300

    refactor: header, search and menu collapse

--- src/shared/prep/modules-prep/hamburger-menu/hamburger-menu.component.tsx
+++ src/shared/prep/modules-prep/hamburger-menu/hamburger-menu.component.tsx
@@ -1,55 +1,23 @@
 import * as React from "react";
 import styles from "./hamburger-menu-component.module.scss";
 import { Link } from "react-router-dom";
-import { IconComponent } from "@app/prep/modules-prep/core";
-import { CLOSE_ICON } from "@app/constants/icons";
 
-export interface IHamburgerMenuComponentProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
+export interface IHamburgerMenuComponentProps {}
 
-const HamburgerMenuComponent = (props: IHamburgerMenuComponentProps) => {
-  const { isOpen } = props;
-
-  const styleOpened = isOpen ? "opened" : "closed";
-
-  React.useEffect(() => {
-    if (props.isOpen) {
-      document.body.style.overflow = "hidden";
-    }
-
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [props.isOpen]);
-
-  return (
-    <div className={`${styles["hamburger-menu"]} ${styles[styleOpened]}`}>
-      <div className={styles["hamburger-menu__holder"]}>
-        <div
-          onClick={props.onClose}
-          role="button"
-          aria-label="closeButton"
-          className={styles["hamburger-menu__close"]}
-        >
-          <IconComponent icon={CLOSE_ICON} size="12px" />
-          Sluiten
-        </div>
-        <ul className={styles["hamburger-menu__navigation"]}>
-          <li>
-            <Link to="./">Winkels</Link>
-          </li>
-          <li>
-            <Link to="./">Productdeals</Link>
-          </li>
-          <li>
-            <Link to="./">Black Friday</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
+const HamburgerMenuComponent = (props: IHamburgerMenuComponentProps) => (
+  <div className={styles["hamburger-menu"]}>
+    <ul className={styles["hamburger-menu__navigation"]}>
+      <li>
+        <Link to="./">Winkels</Link>
+      </li>
+      <li>
+        <Link to="./">Productdeals</Link>
+      </li>
+      <li>
+        <Link to="./">Black Friday</Link>
+      </li>
+    </ul>
+  </div>
+);
 
 export { HamburgerMenuComponent };
--- src/shared/prep/modules-prep/nav-bar/nav-bar.component.tsx
+++ src/shared/prep/modules-prep/nav-bar/nav-bar.component.tsx
@@ -1,78 +1,64 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import styles from "./nav-bar-component.module.scss";
-import { IconComponent, ImageComponent } from "@app/prep/modules-prep/core";
+import { ImageComponent } from "@app/prep/modules-prep/core";
 import { NavbarSearchComponent } from "../navbar-search/navbar-search.component";
 import { HamburgerMenuComponent } from "../hamburger-menu/hamburger-menu.component";
 import { SEARCH_ICON, MENU_ICON } from "@app/constants/icons";
+import { ModalNavBar } from "@app/prep/modules-prep/core/modal-navbar";
 import LOGO from "@assets/bf-expert-brand.svg";
 
 export interface INavBarComponentProps {}
 
-const NavBarComponent = (props: INavBarComponentProps) => {
-  const [isMenuOpened, setIsMenuOpened] = React.useState(false);
-  const [isSearching, setIsSearching] = React.useState(false);
-  const stopSearching = () => {
-    setIsSearching(false);
-  };
-  const toggleOpened = () => {
-    setIsMenuOpened(!isMenuOpened);
-  };
-
-  return (
-    <div className={styles["nav-bar"]}>
-      <div className="uk-container">
-        <div className={styles["nav-bar__holder"]}>
-          <div>
-            <div className={styles["nav-bar__logo"]}>
-              <Link to="./">
-                <ImageComponent src={LOGO} alt="image" isBlocking={true} />
-              </Link>
-            </div>
-            <div className={`uk-visible@s ${styles["nav-bar__label"]}`}>
-              Black Friday 2019: NOG 10 dagen!
-            </div>
+const NavBarComponent = (props: INavBarComponentProps) => (
+  <div className={styles["nav-bar"]}>
+    <div className="uk-container">
+      <div className={styles["nav-bar__holder"]}>
+        <div>
+          <div className={styles["nav-bar__logo"]}>
+            <Link to="./">
+              <ImageComponent src={LOGO} alt="image" isBlocking={true} />
+            </Link>
           </div>
-          <div>
-            <ul className={`uk-visible@m ${styles["nav-bar__navigation"]}`}>
-              <li>
-                <Link to="./">Winkels</Link>
-              </li>
-              <li>
-                <Link to="./">Productdeals</Link>
-              </li>
-              <li>
-                <Link to="./">Black Friday</Link>
-              </li>
-            </ul>
-            <div
-              className={styles["nav-bar__search-button"]}
-              role="button"
-              aria-label="searchButton"
-              onClick={() => {
-                setIsSearching(true);
-              }}
-            >
-              <IconComponent icon={SEARCH_ICON} size="14px" />
-            </div>
-            <div
-              className={styles["nav-bar__toggle"]}
-              role="button"
-              aria-label="menuButton"
-              onClick={() => {
-                setIsMenuOpened(true);
-              }}
-            >
-              <IconComponent icon={MENU_ICON} size="17px" />
-              Menu
-            </div>
+          <div className={`uk-visible@s ${styles["nav-bar__label"]}`}>
+            Black Friday 2019: NOG 10 dagen!
           </div>
         </div>
+        <div>
+          <ul className={`uk-visible@m ${styles["nav-bar__navigation"]}`}>
+            <li>
+              <Link to="./">Winkels</Link>
+            </li>
+            <li>
+              <Link to="./">Productdeals</Link>
+            </li>
+            <li>
+              <Link to="./">Black Friday</Link>
+            </li>
+          </ul>
+
+          <ModalNavBar
+            className={`modal-search ${styles["nav-bar__search-button"]}`}
+            close={"Sluiten"}
+            icon={SEARCH_ICON}
+            iconSize="14px"
+          >
+            <NavbarSearchComponent />
+          </ModalNavBar>
+
+          <ModalNavBar
+            className={`modal-menu ${styles["nav-bar__toggle"]}`}
+            title={"Menu"}
+            close={"Sluiten"}
+            icon={MENU_ICON}
+            iconSize="17px"
+          >
+            <HamburgerMenuComponent />
+          </ModalNavBar>
+        </div>
       </div>
-      <NavbarSearchComponent opened={isSearching} onClose={stopSearching} />
-      <HamburgerMenuComponent isOpen={isMenuOpened} onClose={toggleOpened} />
     </div>
-  );
-};
+  </div>
+);
 
 export { NavBarComponent };
