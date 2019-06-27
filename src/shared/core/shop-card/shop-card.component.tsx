f9a9934b9402574490eb000a89bf0aa3b57ef725
Pedro Knup <phknup@gmail.com>
2019-06-27 10:45:25 +0200

    feat: shop card basics implemented

--- src/shared/core/shop-card/shop-card.component.tsx
+++ src/shared/core/shop-card/shop-card.component.tsx
@@ -1,15 +1,46 @@
 
 import * as React from "react";
 import styles from "./shop-card-component.module.scss";
+import classNames from 'classnames';
+import { TimeLeftBarComponent } from "../time-left-bar";
+import { ImageComponent } from "@app/prep/modules-prep/core";
+import { LinkComponent } from "../link";
+import { getArrow } from "@app/constants/icons";
+
 
 export interface IShopCardComponentProps {
-
+  button_link?: any;
+  button_text?: any;
+  content?: any;
+  image?: any;
+  range?: any;
+  sub_title?: any;
+  title?: any;
+  variant?: string;
 }
 
 const ShopCardComponent = (props: IShopCardComponentProps) => {
-  return ( <div className={styles["shop-card"]}>
+  const { title, sub_title, content, image, range, button_text, button_link, variant } = props;
 
-  </div>)
+  return (
+    <div className={classNames('card-shop', `card-shop--${variant || 'default'}`)}>
+      <div className='card-shop__body'>
+        <div className='card-shop__image'>
+          <ImageComponent src={image} />
+        </div>
+        <TimeLeftBarComponent range={range} title={sub_title} /> 
+        <div className='card-shop__content'>
+          <div className='card-shop__title'>{title}</div>
+          {content}
+        </div>
+      </div>
+      <div className='card-shop__action'>
+        <LinkComponent to={button_link} variant='tertiary' icon={getArrow(false)}>
+          {button_text}
+        </LinkComponent>
+      </div>
+    </div>
+  );
 };
 
 export { ShopCardComponent };
--- src/shared/modules/playground-pedro/playground-pedro.component.tsx
+++ src/shared/modules/playground-pedro/playground-pedro.component.tsx
@@ -3,26 +3,35 @@ import { LinkComponent } from "@app/core/link";
 import { getArrow } from "@app/constants/icons";
 
 import styles from "./playground-pedro-component.module.scss";
+import { TimeLeftBarComponent } from "@app/core/time-left-bar";
+import { ShopCardComponent } from "@app/core/shop-card";
 
 export interface IPlaygroundPedroComponentProps {}
 
 const PlaygroundPedroComponent = (props: IPlaygroundPedroComponentProps) => (
-  <div className={styles["playground-pk"]}>
+  <div className={styles['playground-pk']}>
     Adicione seu código aqui Pedro!
     <i>Wp tom</i>
     <br />
-    <LinkComponent to="/">Primary</LinkComponent>
-    <LinkComponent icon={getArrow(false)} to="/">
+    <LinkComponent to='/'>Primary</LinkComponent>
+    <LinkComponent icon={getArrow(false)} to='/'>
       Primary w Icon
     </LinkComponent>
-    <LinkComponent variant="secondary" icon={getArrow(false)} to="/">
+    <LinkComponent variant='secondary' icon={getArrow(false)} to='/'>
       Secondary w Icon
     </LinkComponent>
-    <LinkComponent animated variant="tertiary" icon={getArrow(false)} to="/">
+    <LinkComponent animated variant='tertiary' icon={getArrow(false)} to='/'>
       Tertiary w Icon animated
     </LinkComponent>
-
-        <TimeLeftBarComponent title="NU GELDIG" range={0.6} />
+    <TimeLeftBarComponent title='NU GELDIG' range={0.6} />
+    <ShopCardComponent
+      image='https://d2ddoduugvun08.cloudfront.net/items/2L2B2V3Q0p2W2C041W1h/Image%202019-06-27%20at%2010.42.50%20AM.png'
+      title='title'
+      button_text='See more'
+      content='content'
+      range='0.7'
+      sub_title='subtitle'
+    />
   </div>
 );
 
