baf7e239aeaf7ff05f6f7402a93fcc02f0bb37df
Martijn Schoenmaker Webbio <martijn@webbio.nl>
2019-06-28 11:21:48 +0200

    feat: fixed button problems with fab and link

--- src/shared/core/button/button.component.tsx
+++ src/shared/core/button/button.component.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import style from "./button-component.module.scss";
-import { Link } from "react-router-dom";
 import classNames from "classnames";
 import { IconComponent } from "@app/core/icon/icon.component";
 
-type ButtonType =
+export type ButtonType =
   | "primary-default"
   | "primary-brand"
   | "primary-inverted"
@@ -18,27 +17,16 @@ interface IButtonProps {
   classNames?: void;
   disabled?: boolean;
   fullWidth?: boolean;
-  href?: string;
   icon?: string;
   iconPosition?: string;
   onClick?: () => void;
   size?: number;
-  target?: string;
   title?: string;
   variant?: string;
 }
 
 const Button = (props: IButtonProps) => {
-  const {
-    href,
-    variant,
-    target,
-    title,
-    buttonType,
-    icon,
-    disabled,
-    iconPosition
-  } = props;
+  const { variant, title, buttonType, icon, disabled, iconPosition } = props;
   const classModify = variant || "primary-default";
   const buttonFAB = !title ? style["button--FAB"] : "";
   const buttonClassName = classNames(
@@ -69,37 +57,25 @@ const Button = (props: IButtonProps) => {
 
   return (
     <React.Fragment>
-      {href ? (
-        <Link
-          style={buttonStyle}
-          to={disabled ? "" : href}
-          className={buttonClassName}
-          target={target}
-        >
-          {title}
-          {icon && <IconComponent icon={icon} size="14px" />}
-        </Link>
-      ) : (
-        <button
-          disabled={disabled}
-          style={buttonStyle}
-          type={buttonType}
-          name={title}
-          className={buttonClassName}
-          onClick={
-            !disabled && typeof props.onClick !== "undefined"
-              ? props.onClick
-              : undefined
-          }
-        >
-          <span className={style["icon-svg"]}>
-            {((icon && iconPosition === "right") || (icon && !iconPosition)) &&
-              title}
-            {icon ? renderIconMargin(iconPosition) : title}
-            {icon && iconPosition === "left" && title}
-          </span>
-        </button>
-      )}
+      <button
+        disabled={disabled}
+        style={buttonStyle}
+        type={buttonType}
+        name={title}
+        className={buttonClassName}
+        onClick={
+          !disabled && typeof props.onClick !== "undefined"
+            ? props.onClick
+            : undefined
+        }
+      >
+        <span className={style["icon-svg"]}>
+          {((icon && iconPosition === "right") || (icon && !iconPosition)) &&
+            title}
+          {icon ? renderIconMargin(iconPosition) : title}
+          {icon && iconPosition === "left" && title}
+        </span>
+      </button>
     </React.Fragment>
   );
 };
