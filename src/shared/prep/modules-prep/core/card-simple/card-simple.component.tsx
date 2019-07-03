9c02282f43a301f77bd45993275db4d29b43c88d
jekatibor <jeka.tibor@gmail.com>
2019-07-03 11:57:48 +0300

    refactor: card simple

--- src/shared/prep/modules-prep/core/card-simple/card-simple.component.tsx
+++ src/shared/prep/modules-prep/core/card-simple/card-simple.component.tsx
@@ -3,23 +3,30 @@ import { Link } from "react-router-dom";
 import "./card-simple-component.scss";
 import { IconComponent, ImageComponent } from "@app/prep/modules-prep/core";
 import { CHEVRON_RIGHT } from "@app/constants/icons";
+import classNames from "classnames";
 
 interface ICardSimpleProps {
-  content?: JSX.Element;
   icon?: string;
   link: string;
   title?: string;
+  variant?: string;
 }
 
-const CardSimple = ({ title, icon, link }: ICardSimpleProps) => (
-  <Link to={link} className="card-simple">
-    {icon && (
-      <div className="card-simple__image">
-        <ImageComponent src={icon} />
-      </div>
-    )}
-    <div className="card-simple__title">{title}</div>
-    <IconComponent icon={CHEVRON_RIGHT} size="5px" />
-  </Link>
-);
+const CardSimple = ({ title, icon, link, variant }: ICardSimpleProps) => {
+  const buttonClassName = classNames("card-simple", {
+    [`card-simple--${variant}`]: variant
+  });
+
+  return (
+    <Link to={link} className={buttonClassName}>
+      {icon && (
+        <div className="card-simple__image">
+          <ImageComponent src={icon} />
+        </div>
+      )}
+      <div className="card-simple__title">{title}</div>
+      <IconComponent icon={CHEVRON_RIGHT} size="5px" />
+    </Link>
+  );
+};
 export { CardSimple };
