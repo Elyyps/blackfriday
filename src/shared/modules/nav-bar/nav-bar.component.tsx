e7c4597aa5be0af94cb8d8f059c53849091c535f
Pedro Knup <phknup@gmail.com>
2019-07-03 15:35:06 +0200

    feat: finished component

--- src/shared/modules/nav-bar/nav-bar.component.tsx
+++ src/shared/modules/nav-bar/nav-bar.component.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import styles from "./nav-bar-component.module.scss";
 import { ImageComponent } from "@app/prep/modules-prep/core";
-
 import { SEARCH_ICON, MENU_ICON } from "@app/constants/icons";
 import { ModalNavBar } from "@app/prep/modules-prep/core/modal-navbar";
-import LOGO from "@assets/bf-expert-brand.svg";
-import { NavbarSearchComponent } from "@app/prep/modules-prep/navbar-search";
 import { HamburgerMenuComponent } from "@app/prep/modules-prep/hamburger-menu";
+import { LinkComponent } from "@app/core/link";
+import { INavBarModule } from "@app/api/modules/navbar/navbar.module";
 
-export interface INavBarComponentProps {}
+export interface INavBarComponentProps {
+  navBarModule: INavBarModule;
+}
 
 const NavBarComponent = (props: INavBarComponentProps) => (
   <div className={styles["nav-bar"]}>
@@ -17,44 +17,36 @@ const NavBarComponent = (props: INavBarComponentProps) => (
       <div className={styles["nav-bar__holder"]}>
         <div>
           <div className={styles["nav-bar__logo"]}>
-            <Link to="./">
-              <ImageComponent src={LOGO} alt="image" isBlocking={true} />
-            </Link>
+            <LinkComponent to="./">
+              <ImageComponent src={props.navBarModule.logo} alt="image" isBlocking={true} />
+            </LinkComponent>
           </div>
-          <div className={`uk-visible@s ${styles["nav-bar__label"]}`}>
-            Black Friday 2019: NOG 10 dagen!
-          </div>
+          <div className={`uk-visible@s ${styles["nav-bar__label"]}`}>{props.navBarModule.label}</div>
         </div>
         <div>
           <ul className={`uk-visible@m ${styles["nav-bar__navigation"]}`}>
-            <li>
-              <Link to="./">Winkels</Link>
-            </li>
-            <li>
-              <Link to="./">Productdeals</Link>
-            </li>
-            <li>
-              <Link to="./">Black Friday</Link>
-            </li>
+            {props.navBarModule.links.map((link, key) => (
+              <li key={key}>
+                <LinkComponent to={link.url}>{link.title}</LinkComponent>
+              </li>
+            ))}
           </ul>
 
           <ModalNavBar
             className={`modal-search ${styles["nav-bar__search-button"]}`}
-            close={"Sluiten"}
+            close={props.navBarModule.closeText}
             icon={SEARCH_ICON}
             iconSize="14px"
-          >
-            <NavbarSearchComponent />
-          </ModalNavBar>
+          />
 
           <ModalNavBar
             className={`modal-menu ${styles["nav-bar__toggle"]}`}
             title={"Menu"}
-            close={"Sluiten"}
+            close={props.navBarModule.closeText}
             icon={MENU_ICON}
             iconSize="17px"
           >
-            <HamburgerMenuComponent/>
+            <HamburgerMenuComponent />
           </ModalNavBar>
         </div>
       </div>
--- src/shared/modules/playground-pedro/playground-pedro.component.tsx
+++ src/shared/modules/playground-pedro/playground-pedro.component.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
-import { LinkComponent } from "@app/core/link";
-import { getArrow } from "@app/constants/icons";
-
 import styles from "./playground-pedro-component.module.scss";
-import { SeeMoreCardComponent } from "@app/core/see-more-card";
 import { NavBarComponent } from "../nav-bar";
+import { generateDummyNavbarData } from "@app/api/modules/navbar/generate-dummy-data";
 
 export interface IPlaygroundPedroComponentProps {}
 
 const PlaygroundPedroComponent = (props: IPlaygroundPedroComponentProps) => (
   <div className={styles["playground-pk"]}>
-    <NavBarComponent />
+    <NavBarComponent navBarModule={generateDummyNavbarData()} />
   </div>
 );
 
