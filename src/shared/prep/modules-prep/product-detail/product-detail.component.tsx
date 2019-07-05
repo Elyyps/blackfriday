8862db3183f66933cfb7e9d1cf81d2920ad78ad8
Alexander Tkachenko <sanya_90_2011@mail.ru>
2019-07-05 10:40:48 +0300

    feat: finished winkle single page

--- src/shared/prep/modules-prep/product-detail/product-detail.component.tsx
+++ src/shared/prep/modules-prep/product-detail/product-detail.component.tsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 import "./product-detail-component.scss";
-import { Cards } from "@app/prep/pages-prep/winkel-single/dummy-data";
+import { Card } from "@app/prep/pages-prep/winkel-single/dummy-data";
 import { Button, CardShop, IconComponent } from "@app/prep/modules-prep/core";
 import { RatingComponent } from "./rating.component";
 import { Link } from "react-router-dom";
 
 export interface IProductDetailComponentComponentProps {
   breadcrumbs?: any;
-  cards: object;
   content?: string;
   image?: string;
   labelList?: any;
@@ -21,34 +20,35 @@ const ProductDetailComponent = ({
   content,
   image,
   labelList,
-  cards,
   rating,
   breadcrumbs,
   list
 }: IProductDetailComponentComponentProps) => (
   <div className="product-detail">
     <div className="uk-container">
-      <div className="uk-grid uk-grid">
+      <div className="uk-grid uk-grid-small">
         <div className="uk-width-1-3@s">
-          {Cards &&
-            Cards.map((item, key) => (
-              <>
-                <CardShop key={key} {...item} />
-                <div className="card-message">
-                  <div className="card__head">
-                    <IconComponent icon={item.message.icon} size={"30px"} />
-                  </div>
-                  <div className="card__body">
-                    <div className="card__content">
-                      {item.message.description}
+          <div className="card-wrap">
+            {Card &&
+              Card.map((item, key) => (
+                <>
+                  <CardShop key={key} {...item} />
+                  <div className="card-message uk-visible@s">
+                    <div className="card__head">
+                      <IconComponent icon={item.message.icon} size={"30px"} />
                     </div>
-                    <div className="card__action">
-                      <Button {...item.message.button} />
+                    <div className="card__body">
+                      <div className="card__content">
+                        {item.message.description}
+                      </div>
+                      <div className="card__action">
+                        <Button {...item.message.button} />
+                      </div>
                     </div>
                   </div>
-                </div>
-              </>
-            ))}
+                </>
+              ))}
+          </div>
         </div>
         <div className="uk-width-2-3@s">
           <div className="product-detail__body">
@@ -83,7 +83,7 @@ const ProductDetailComponent = ({
                 </ul>
               </div>
               <div className="product-detail__footer">
-                <div className="actions">
+                <div className="actions" data-uk-margin>
                   <div className="actions__item">
                     <Button href={"#"} title={"Beddenreus"} variant={"link"} />
                     <div className="uk-hidden@s">
@@ -105,7 +105,7 @@ const ProductDetailComponent = ({
                     </div>
                   </div>
                 </div>
-                <div className="labels">
+                <div className="labels uk-visible@s">
                   {labelList
                     ? labelList.map((item: any, key: number) => (
                         <div key={key} className="labels__item">
@@ -117,6 +117,24 @@ const ProductDetailComponent = ({
               </div>
             </div>
           </div>
+          {Card &&
+            Card.map((item, key) => (
+              <>
+                <div className="card-message uk-hidden@s">
+                  <div className="card__head">
+                    <IconComponent icon={item.message.icon} size={"30px"} />
+                  </div>
+                  <div className="card__body">
+                    <div className="card__content">
+                      {item.message.description}
+                    </div>
+                    <div className="card__action">
+                      <Button {...item.message.button} />
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
       </div>
     </div>
