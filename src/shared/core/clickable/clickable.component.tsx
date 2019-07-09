28377aad199fffa7ab491fe2c16f826f45cf4b70
Martijn Schoenmaker Webbio <martijn@webbio.nl>
2019-07-09 15:14:33 +0200

    feat: finished clickable component

---
+++ src/shared/core/clickable/clickable.component.tsx
@@ -0,0 +1,124 @@
+import * as React from "react";
+import styles from "./clickable-component.module.scss";
+import { Link } from "react-router-dom";
+import { IconComponent } from "@app/core/icon";
+import classNames from "classnames";
+
+export type ButtonVariant =
+  | "primary-default"
+  | "primary-brand"
+  | "primary-inverted"
+  | "secondary"
+  | "secondary-inverted"
+  | "tertiary"
+  | "tertiary-inverted"
+  | "link-primary"
+  | "link-secondary"
+  | "link-tertiary";
+
+export type IconStyle = "outline" | "filled" | "outline-fill";
+
+export interface IClickableComponentProps {
+  animated?: boolean;
+  buttonType?: string;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  href?: string;
+  icon?: string;
+  iconPosition?: string;
+  iconStyle?: IconStyle;
+  onClick?: () => void;
+  size?: number;
+  target?: "_blank" | "_self";
+  title?: string;
+  variant?: ButtonVariant;
+}
+
+const renderIconMargin = (margin = "right", title: string | undefined, icon: string | undefined) => {
+  let newMargin = "";
+  if (title) {
+    newMargin = margin;
+  }
+
+  return (
+    <span className={styles[`icon-${newMargin}`]}>
+      <IconComponent icon={icon} size="14px" />
+    </span>
+  );
+};
+
+const renderIconText = (icon: string | undefined, iconPosition: string | undefined, title: string | undefined) => (
+  <span className={styles["icon-svg"]}>
+    {((icon && iconPosition === "right") || (icon && !iconPosition)) && title}
+    {icon ? renderIconMargin(iconPosition, title, icon) : title}
+    {icon && iconPosition === "left" && title}
+  </span>
+);
+
+const ClickableComponent = (props: IClickableComponentProps) => {
+  const {
+    buttonType,
+    animated,
+    onClick,
+    href,
+    variant,
+    title,
+    icon,
+    disabled,
+    iconStyle,
+    iconPosition,
+    target
+  } = props;
+  const classModify = variant || "primary-default";
+  const buttonFAB = !title ? styles["button--FAB"] : "";
+  const animatedIcon = animated ? styles[`button--animated`] : "";
+  const iconOutline = iconStyle ? styles[`button--${classModify}--icon-${iconStyle}`] : "";
+  const buttonClassName = classNames(
+    styles["button"],
+    styles[`button--${classModify}`],
+    buttonFAB,
+    iconOutline,
+    animatedIcon,
+    {
+      "button--fullWidth": props.fullWidth
+    }
+  );
+  const buttonStyle = {
+    width: props.fullWidth ? "100%" : props.size,
+    height: props.size
+  };
+
+  if (href) {
+    const regex = "https://|http://";
+    const isExternalLink = href.match(regex) !== null ? true : false;
+
+    if (isExternalLink) {
+      return (
+        <a target={target || "_blank"} className={buttonClassName} style={buttonStyle} href={href}>
+          {renderIconText(icon, iconPosition, title)}
+        </a>
+      );
+    }
+
+    return (
+      <Link target={target || "_self"} className={buttonClassName} style={buttonStyle} to={href}>
+        {renderIconText(icon, iconPosition, title)}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      style={buttonStyle}
+      type={buttonType}
+      name={title}
+      className={buttonClassName}
+      onClick={onClick}
+    >
+      <span className={styles["icon-svg"]}>{renderIconText(icon, iconPosition, title)}</span>
+    </button>
+  );
+};
+
+export { ClickableComponent };
