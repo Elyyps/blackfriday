6b7dfc74f80ae21670a9d51dd4497ab6c25c0695
elyyps <ahmed@webbio.nl>
2019-07-23 14:51:30 +0200

    feat: finished select component

--- src/shared/core/select/select.component.spec.tsx
+++ src/shared/core/select/select.component.spec.tsx
@@ -1,15 +1,10 @@
-
 import React from "react";
 import { shallow } from "enzyme";
 import { SelectComponent } from "./select.component";
-
+const options = ["newest", "price", "relevance"];
 describe("[Select]", () => {
   it("should render component without crashing", () => {
-    const renderedComponent = shallow(
-      <SelectComponent />
-    );
+    const renderedComponent = shallow(<SelectComponent options={options} onSelect={() => ""} />);
     expect(renderedComponent).toMatchSnapshot();
   });
 });
-
-  
--- src/shared/core/select/select.stories.tsx
+++ src/shared/core/select/select.stories.tsx
@@ -1,16 +1,12 @@
-
 import * as React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withProvider } from "@app/util";
 import { SelectComponent } from "./select.component";
+const options = ["newest", "price", "relevance"];
 
 storiesOf("Select", module)
   .addDecorator(withA11y)
   .addDecorator(withProvider)
-  .add("Basic implementation", () => (
-    <SelectComponent />
-  ));
-
-  
+  .add("Basic implementation", () => <SelectComponent options={options} onSelect={() => ""} />);
