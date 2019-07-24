2827af0da4d24ebc53561c821b048305390d5919
Pedro Knup <phknup@gmail.com>
2019-07-24 09:24:30 +0200

    fix: fixed lint warning

--- src/shared/api/modules/featured-shops/featured-shops.module.ts
+++ src/shared/api/modules/featured-shops/featured-shops.module.ts
@@ -1,4 +1,3 @@
-import { ShopCard } from "@app/api/core/shop-card/shop-card";
 import { SeeMoreCard } from "@app/api/core/see-more-card";
 import { WordPressPostModule } from "../wordpress-module/wordpress-module";
 import { ShopCardModule } from "@app/api/core/shop-card";
