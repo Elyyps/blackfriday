802da370a616fa37f285e922ba9cb3499517511b
elyyps <ahmed@webbio.nl>
2019-07-24 16:19:34 +0200

    feat: merge

--- src/shared/core/mobile-filter/mobile-filter.component.spec.tsx
+++ src/shared/core/mobile-filter/mobile-filter.component.spec.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { MobileFilterComponent } from "./mobile-filter.component";
 import { mobileFilterDummyData } from "./dummy-data";
-import { shallow } from "enzyme";
+import { shallowWithIntl } from "enzyme-react-intl";
 
 describe("[MobileFilter]", () => {
   it("should render component without crashing", () => {
-    const renderedComponent = shallow(
+    const renderedComponent = shallowWithIntl(
       <MobileFilterComponent totalStores={0} onClear={jest.fn} onClose={jest.fn} filterItems={mobileFilterDummyData} />
     );
     expect(renderedComponent).toMatchSnapshot();
