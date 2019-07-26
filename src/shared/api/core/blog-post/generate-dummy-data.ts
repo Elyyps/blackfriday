49ec4669626b44b7d7fa19120f8138af0eed3dfb
Ahmed-Mabrook <ahmed@webbio.nl>
2019-07-26 15:51:02 +0200

    feat: added featured blog

--- src/shared/api/core/blog-post/generate-dummy-data.ts
+++ src/shared/api/core/blog-post/generate-dummy-data.ts
@@ -32,3 +32,20 @@ export const generateDummyBlogPost = (): BlogPost[] => [
     link: { title: "Lees meer", url: "" }
   }
 ];
+export const generate2DummyBlogPosts = (): BlogPost[] => [
+  {
+    image: "https://viper-development-images.s3-eu-west-1.amazonaws.com/blackFriday/backFridayAd.png",
+    title: "Vijf voorspellingen van Black Friday Nederland",
+    content:
+      "Nog even en alles wordt weer uit de kast gehaald voor de beste Black Friday deals van 2018. Maar voordat het zo ver is, kijken we nog even terug... ",
+    link: { title: "Lees meer", url: "" }
+  },
+  {
+    image: "https://viper-development-images.s3-eu-west-1.amazonaws.com/blackFriday/backFridayAd.png",
+    title: "Top Black Friday deals van vorig jaar",
+    content:
+      "Nog even en alles wordt weer uit de kast gehaald voor de beste Black Friday deals van 2018. Maar voordat het zo ver is, kijken we nog even terug... ",
+
+    link: { title: "Lees meer", url: "" }
+  }
+];
