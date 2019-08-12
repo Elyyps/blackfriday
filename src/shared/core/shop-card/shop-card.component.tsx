066c0f3da88a598f9d3dba4d99fa4e8f67aabed6
Martijn Schoenmaker Webbio <martijn@webbio.nl>
2019-08-12 16:20:17 +0200

    fix: multiple fixes styling shop card

--- src/shared/core/shop-card/shop-card.component.tsx
+++ src/shared/core/shop-card/shop-card.component.tsx
@@ -10,6 +10,8 @@ import { ClickableComponent } from "../clickable";
 import { injectIntl, InjectedIntlProps } from "react-intl";
 import { IconComponent } from "../icon";
 import IconHot from "@assets/icons/hot.svg";
+import { Link } from "react-router-dom";
+import { getStatusBarColor } from "@app/util/get-status-bar-color";
 
 export interface IShopCardComponentProps {
   store: Store;
@@ -20,43 +22,35 @@ const component = (props: IShopCardComponentProps & InjectedIntlProps) => {
   const { description, logo, moreInfoLink, name, status, timeLeftPercentage, label } = props.store;
   const shopCardVariant = props.variant || "default";
 
-  const getStatusBarColor = () => {
-    const rangeNumber = timeLeftPercentage;
-    const limit = 50;
-    if (rangeNumber === 0 || typeof rangeNumber === "undefined") {
-      return "none";
-    }
-
-    if (rangeNumber <= limit) {
-      return "orange";
-    }
-
-    return "green";
-  };
-
   return (
     <ShadowCardComponent fullWidth>
       <div className={`${styles["shop-card"]} ${styles[`shop-card--${shopCardVariant}`]}`}>
         {label && (
-          <div className={styles["shop-card__label"]}>
+          <div className={styles["label"]}>
             <IconComponent icon={IconHot} fillColor="white" size={"12px"} />
             <span>{label}</span>
           </div>
         )}
-        <div className={styles["shop-card__body"]}>
-          <div className={styles["shop-card__image"]}>
-            <ImageComponent alt="Shop logo" src={logo} isBlocking />
+        <div className={styles["body"]}>
+          <div className={styles["image"]}>
+            <Link to="/stores-single">
+              <ImageComponent alt="Shop logo" src={logo} isBlocking />
+            </Link>
           </div>
-          <div className={`${styles["shop-card__status-title"]} ${styles[getStatusBarColor()]}`}>
+          <div className={`${styles["status-title"]} ${styles[getStatusBarColor(timeLeftPercentage)]}`}>
             {getStoreStatusText(status)}
           </div>
-          <div className={styles["shop-card__bar"]}>
-            <TimeLeftBarComponent variant="responsive" color={getStatusBarColor()} range={timeLeftPercentage} />
+          <div className={styles["bar"]}>
+            <TimeLeftBarComponent
+              variant="responsive"
+              color={getStatusBarColor(timeLeftPercentage)}
+              range={timeLeftPercentage}
+            />
           </div>
 
-          <div className={styles["shop-card__content"]}>
-            <div className={styles["shop-card__title"]}>{name}</div>
-            <span className={styles["shop-card__description"]}>{description}</span>
+          <div className={styles["content"]}>
+            <div className={styles["title"]}>{name}</div>
+            <span className={styles["description"]}>{description}</span>
             <ClickableComponent
               href="/stores-single"
               title={props.intl.formatMessage({ id: "see-more" })}
@@ -64,7 +58,7 @@ const component = (props: IShopCardComponentProps & InjectedIntlProps) => {
             />
           </div>
         </div>
-        <div className={styles["shop-card__action"]}>
+        <div className={styles["action"]}>
           <ClickableComponent
             iconStyle="filled"
             dynamicSize={true}
