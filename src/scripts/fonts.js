58ba832b8c5bf62b7b48577c0480a1a96bf602e2
Daniele acquaviva <danieleacquaviva5@gmail.com>
2019-08-16 15:16:06 +0200

    feat: fixed bug ie

--- src/scripts/fonts.js
+++ src/scripts/fonts.js
@@ -1,7 +1,7 @@
 (function() {
   WebFont.load({
     google: {
-      families: ["Assistant: 300, 400, 600, 700, 800", "Bai Jamjuree: 300, 400, 500, 600, 700"]
+      families: ["Assistant: 300, 400, 600, 700, 800", "Bai Jamjuree, sans-serif: 300, 400, 500, 600, 700"]
     }
   });
 })();
