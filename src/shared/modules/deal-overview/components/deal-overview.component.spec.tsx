3ad89edaf9639d9b67fd7462904f235c61462410
Pedro Knup <phknup@gmail.com>
2019-08-21 09:26:05 +0200

    feat: implemented tests and storybook documentation

--- src/shared/modules/deal-overview/components/deal-overview.component.spec.tsx
+++ src/shared/modules/deal-overview/components/deal-overview.component.spec.tsx
@@ -1,61 +1,47 @@
-// import React from "react";
-// import { shallowWithIntl } from "enzyme-react-intl";
+import React from "react";
+import { shallowWithIntl } from "enzyme-react-intl";
+import { ViewType } from "@app/stores/settings";
+import { DealOverview, getTotalHeight } from "./deal-overview.component";
+import { generateDealOverviewModule } from "@app/api/modules/deal-overview/generate-dummy-data";
 
-// import { StoreOverview, getTotalHeight } from "@app/modules/store-overview/components/store-overview.component";
-// import { generateStoreOverviewModule } from "@app/api/modules/store-overview/generate-dummy-data";
-// import { ViewType } from "@app/stores/settings";
-// /* tslint:disable */
-// describe("[StoreOverview]", () => {
-//   it("should render without crashing", () => {
-//     const renderedComponent = shallowWithIntl(
-//       <StoreOverview
-//         brandFilterItems={[]}
-//         categoryFilterItems={[]}
-//         screenSize={{ breakpointPixels: 0, viewType: ViewType.Desktop }}
-//         sortBy=""
-//         statusFilterItems={[]}
-//         stores={[]}
-//         totalResults={100}
-//         storeOverviewModule={generateStoreOverviewModule()}
-//         getStores={jest.fn()}
-//         setBrandFilters={jest.fn()}
-//         setCategoryFilters={jest.fn()}
-//         setSortBy={jest.fn()}
-//         setStatusFilters={jest.fn()}
-//         clearAllFilters={jest.fn()}
-//       />
-//     );
-//     expect(renderedComponent).toMatchSnapshot();
-//   });
-
-//   it("should get proper total height for many stores", () => {
-//     const totalHeight = getTotalHeight(ViewType.DesktopFull, 200);
-//     expect(totalHeight).toBe(11520);
-//   });
-//   it("should get proper total height for many stores mobile", () => {
-//     const totalHeight = getTotalHeight(ViewType.Mobile, 200);
-//     expect(totalHeight).toBe(33600);
-//   });
-//   it("should get proper total height for few stores", () => {
-//     const totalHeight = getTotalHeight(ViewType.DesktopFull, 5);
-//     expect(totalHeight).toBe(288);
-//   });
-//   it("should get proper total height for few stores mobile", () => {
-//     const totalHeight = getTotalHeight(ViewType.Mobile, 5);
-//     expect(totalHeight).toBe(840);
-//   });
-// });
-
-// import React from "react";
-// import { shallowWithIntl } from "enzyme-react-intl";
-
-// import { StoreOverview, getTotalHeight } from "@app/modules/store-overview/components/store-overview.component";
-// import { generateStoreOverviewModule } from "@app/api/modules/store-overview/generate-dummy-data";
-// import { ViewType } from "@app/stores/settings";
 /* tslint:disable */
-describe("[StoreOverview]", () => {
+describe("[DealsOverview]", () => {
   it("should render without crashing", () => {
-    // const renderedComponent = shallowWithIntl(    );
-    // expect(renderedComponent).toMatchSnapshot();
+    const renderedComponent = shallowWithIntl(
+      <DealOverview
+        brandFilterItems={[]}
+        categoryFilterItems={[]}
+        screenSize={{ breakpointPixels: 0, viewType: ViewType.Desktop }}
+        sortBy=""
+        deals={[]}
+        totalResults={100}
+        dealOverviewModule={generateDealOverviewModule()}
+        setBrandFilters={jest.fn()}
+        setCategoryFilters={jest.fn()}
+        setStoreFilters={jest.fn()}
+        storeFilterItems={[]}
+        getDeals={jest.fn()}
+        clearAllFilters={jest.fn()}
+        setSortBy={jest.fn()}
+      />
+    );
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it("should get proper total height for many stores", () => {
+    const totalHeight = getTotalHeight(ViewType.DesktopFull, 200);
+    expect(totalHeight).toBe(11520);
+  });
+  it("should get proper total height for many stores mobile", () => {
+    const totalHeight = getTotalHeight(ViewType.Mobile, 200);
+    expect(totalHeight).toBe(33600);
+  });
+  it("should get proper total height for few stores", () => {
+    const totalHeight = getTotalHeight(ViewType.DesktopFull, 5);
+    expect(totalHeight).toBe(288);
+  });
+  it("should get proper total height for few stores mobile", () => {
+    const totalHeight = getTotalHeight(ViewType.Mobile, 5);
+    expect(totalHeight).toBe(840);
   });
 });
--- src/shared/modules/deal-overview/components/deal-overview.stories.tsx
+++ src/shared/modules/deal-overview/components/deal-overview.stories.tsx
@@ -1,30 +1,29 @@
-// import * as React from "react";
-
-// import { storiesOf } from "@storybook/react";
-// import { withA11y } from "@storybook/addon-a11y";
-// import { withProvider } from "@app/util";
-// import { StoreOverview } from "./store-overview.component";
-// import { ViewType } from "@app/stores/settings";
-// import { generateStoreOverviewModule } from "@app/api/modules/store-overview/generate-dummy-data";
-// /* tslint:disable */
-// storiesOf("Store overview", module)
-//   .addDecorator(withA11y)
-//   .addDecorator(withProvider)
-//   .add("Basic implementation", () => (
-//     <StoreOverview
-//       brandFilterItems={[]}
-//       categoryFilterItems={[]}
-//       screenSize={{ breakpointPixels: 0, viewType: ViewType.Desktop }}
-//       sortBy=""
-//       statusFilterItems={[]}
-//       stores={[]}
-//       totalResults={100}
-//       storeOverviewModule={generateStoreOverviewModule()}
-//       getStores={() => {}}
-//       setBrandFilters={() => {}}
-//       setCategoryFilters={() => {}}
-//       setSortBy={() => {}}
-//       setStatusFilters={() => {}}
-//       clearAllFilters={() => {}}
-//     />
-//   ));
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+import { withProvider } from "@app/util";
+import { ViewType } from "@app/stores/settings";
+import { DealOverview } from "./deal-overview.component";
+import { generateDealOverviewModule } from "@app/api/modules/deal-overview/generate-dummy-data";
+/* tslint:disable */
+storiesOf("Deal overview", module)
+  .addDecorator(withA11y)
+  .addDecorator(withProvider)
+  .add("Basic implementation", () => (
+    <DealOverview
+      brandFilterItems={[]}
+      categoryFilterItems={[]}
+      screenSize={{ breakpointPixels: 0, viewType: ViewType.Desktop }}
+      sortBy=""
+      deals={[]}
+      totalResults={100}
+      dealOverviewModule={generateDealOverviewModule()}
+      setBrandFilters={jest.fn()}
+      setCategoryFilters={jest.fn()}
+      setStoreFilters={jest.fn()}
+      storeFilterItems={[]}
+      getDeals={jest.fn()}
+      clearAllFilters={jest.fn()}
+      setSortBy={jest.fn()}
+    />
+  ));
